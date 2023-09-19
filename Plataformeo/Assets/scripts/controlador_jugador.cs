24d9f2e6ae045c5bef39ca07909d556003699c9b
carlosartt <142039216+carlosartt@users.noreply.github.com>
2023-09-19 14:35:31 -0600

    Escenario

--- Plataformeo/Assets/scripts/controlador_jugador.cs
+++ Plataformeo/Assets/scripts/controlador_jugador.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class controlador_jugador : MonoBehaviour
 {
     public float velocidadCaminar = 5;
+    public float fuerzaSalto = 20;
 
     private Rigidbody2D MiCuerpo;
     private Animator MiAnimador;
@@ -14,14 +15,16 @@ public class controlador_jugador : MonoBehaviour
         MiCuerpo = GetComponent<Rigidbody2D>();
         MiAnimador = GetComponent<Animator>();
         
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
         float velVert = MiCuerpo.velocity.y;
         float movHoriz = Input.GetAxis("Horizontal");
+        
 
         if (movHoriz > 0)//Se mueve a la der
         {
@@ -43,5 +46,15 @@ public class controlador_jugador : MonoBehaviour
             MiCuerpo.velocity = new Vector3(0, velVert, 0);
             MiAnimador.SetBool("caminata", false);
         }
-    }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            MiCuerpo.AddForce(new Vector3(0,fuerzaSalto,0), ForceMode2D.Impulse);
+        }
+
+        MiAnimador.SetFloat("Vel_Vert", velVert);
+    } 
+    
+   
 }
+
