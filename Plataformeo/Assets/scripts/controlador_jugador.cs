a805d1eb37a764cb9a3f65bc4704a3530abc665a
carlosartt <142039216+carlosartt@users.noreply.github.com>
2023-09-22 04:45:07 -0600

    Proyecto cavernicola completo

--- Plataformeo/Assets/scripts/controlador_jugador.cs
+++ Plataformeo/Assets/scripts/controlador_jugador.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class controlador_jugador : MonoBehaviour
 {
     public float velocidadCaminar = 5;
+    public float fuerzaSalto = 20;
 
     private Rigidbody2D MiCuerpo;
     private Animator MiAnimador;
@@ -23,25 +24,32 @@ public class controlador_jugador : MonoBehaviour
         float velVert = MiCuerpo.velocity.y;
         float movHoriz = Input.GetAxis("Horizontal");
 
-        if (movHoriz > 0)//Se mueve a la der
+        if (movHoriz > 0)//Se mueve a la derecha
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
 
             MiCuerpo.velocity = new Vector3(velocidadCaminar, velVert, 0);
 
-            MiAnimador.SetBool("caminata", true);
+            MiAnimador.SetBool("CAMINANDO", true);
         }
         else if (movHoriz < 0) //Se mueve a la izq
         {
             transform.rotation = Quaternion.Euler(0, 180, 0);
 
             MiCuerpo.velocity = new Vector3(-velocidadCaminar, velVert, 0);
-            MiAnimador.SetBool("caminata", true);
+            MiAnimador.SetBool("CAMINANDO", true);
         }
         else
         {
             MiCuerpo.velocity = new Vector3(0, velVert, 0);
-            MiAnimador.SetBool("caminata", false);
+            MiAnimador.SetBool("CAMINANDO", false);
         }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            MiCuerpo.AddForce(new Vector3(0, fuerzaSalto, 0), ForceMode2D.Impulse);
+        }
+
+        MiAnimador.SetFloat("VEL_VERT", velVert);
     }
 }
